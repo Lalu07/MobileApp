d735ded637e14fe04f38ce657e1a534b6f46f22b
Lalu Muhammad Rizkika Firdaus <iki070899@gmail.com>
2021-03-23 18:25:38 +0800

    Validasi buat data kosong

--- app/src/main/java/firdaus/rizkika/constraintlayout/Pendaftaran.java
+++ app/src/main/java/firdaus/rizkika/constraintlayout/Pendaftaran.java
@@ -7,12 +7,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class Pendaftaran extends AppCompatActivity {
+    EditText edtNama, edtAlamat, edtEmail, edtPassword, edtrePass;
+    FloatingActionButton simpan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pendaftaran);
+
+        simpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (edtNama.getText().toString().isEmpty() || edtAlamat.getText().toString().isEmpty() ||
+                edtEmail.getText().toString().isEmpty() || edtPassword.getText().toString().isEmpty()||
+                edtrePass.getText().toString().isEmpty()){
+
+                    Toast.makeText(Pendaftaran.this,"Seluruh data harus di isi!!",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
+
 }
