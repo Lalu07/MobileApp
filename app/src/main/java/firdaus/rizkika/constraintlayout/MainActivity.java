50c52d42a767de7aeb2b45ce15d7df4e196cd016
Lalu Muhammad Rizkika Firdaus <iki070899@gmail.com>
2021-03-23 18:12:50 +0800

    tambah menu dan form Pendaftaran

--- app/src/main/java/firdaus/rizkika/constraintlayout/MainActivity.java
+++ app/src/main/java/firdaus/rizkika/constraintlayout/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -42,6 +44,23 @@ public class MainActivity extends AppCompatActivity {
                 validasi();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.mnDaftar){
+            Intent i = new Intent(MainActivity.this,Pendaftaran.class);
+            startActivity(i);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 
---
+++ app/src/main/java/firdaus/rizkika/constraintlayout/Pendaftaran.java
@@ -0,0 +1,18 @@
+package firdaus.rizkika.constraintlayout;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Pendaftaran extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pendaftaran);
+    }
+}
